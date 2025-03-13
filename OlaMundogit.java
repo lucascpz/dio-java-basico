6758fb178e90c4de00a16839cbacf463b217bf40
lucascpz <lucascpz@hotmail.com>
2025-03-13 11:45:53 -0300

    Adicionado data no java

--- OlaMundogit.java
+++ OlaMundogit.java
@@ -1,5 +1,5 @@
 public class OlaMundogit {
     public static void main (String[] args) throws  Exception {
-        System.out.println("Ola Mundo!");
+        System.out.println("Ola Mundo!dia 13");
     }
 }
